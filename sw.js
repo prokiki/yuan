09dff8f13cea54d617c7f8588f83b9f6fe93def8
prokiki <44457658+prokiki@users.noreply.github.com>
2025-08-09 21:42:52 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+
+const CACHE_NAME = 'family-reward-cache-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(ASSETS);
+  })());
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const cached = await caches.match(e.request);
+    return cached || fetch(e.request);
+  })());
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
